9ba3df1b2983f5e5b19bcdddc3b2fb45c6da7f9a
Roman Fedotov <67165523+fedotovroman@users.noreply.github.com>
2022-06-24 20:10:05 +0300

    clone from main

---
+++ bayesseq/bayesseq.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class SeqRegressor(nn.Module):
+
+    def __init__(self, n_features, output_length, batch_size):
+
+        super(SeqRegressor, self).__init__()
+
+        self.batch_size = batch_size
+
+        self.hidden_size_1 = 128
+        self.hidden_size_2 = 32
+        self.stacked_layers = 2
+        self.dropout_probability = 0.5
+
+        self.lstm1 = nn.LSTM(n_features,
+                             self.hidden_size_1,
+                             num_layers=self.stacked_layers,
+                             batch_first=True)
+        self.lstm2 = nn.LSTM(self.hidden_size_1,
+                             self.hidden_size_2,
+                             num_layers=self.stacked_layers,
+                             batch_first=True)
+
+        self.fc = nn.Linear(self.hidden_size_2, output_length)
+        self.loss_fn = nn.MSELoss()
+
+    def forward(self, x):
+        batch_size, seq_len, _ = x.size()
+
+        hidden = self.init_hidden1(batch_size)
+        output, _ = self.lstm1(x, hidden)
+        output = F.dropout(output, p=self.dropout_probability, training=True)
+        state = self.init_hidden2(batch_size)
+        output, state = self.lstm2(output, state)
+        output = F.dropout(output, p=self.dropout_probability, training=True)
+        output = output[:, -1, :]
+        y_pred = self.fc(output)
+        return y_pred
+
+    def init_hidden1(self, batch_size):
+        hidden_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_1))
+        cell_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_1))
+        return hidden_state, cell_state
+
+    def init_hidden2(self, batch_size):
+        hidden_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_2))
+        cell_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_2))
+        return hidden_state, cell_state
+
+    def loss(self, pred, truth):
+        return self.loss_fn(pred, truth)
+
+    def predict(self, X):
+        return self(torch.tensor(X, dtype=torch.float32)).view(-1).detach().numpy()
+
+class SeqClassifier(nn.Module):
+
+    def __init__(self, n_features, output_length, batch_size):
+
+        super(SeqClassifier, self).__init__()
+
+        self.batch_size = batch_size
+
+        self.hidden_size_1 = 128
+        self.hidden_size_2 = 32
+        self.stacked_layers = 2
+        self.dropout_probability = 0.5
+
+        self.lstm1 = nn.LSTM(n_features,
+                             self.hidden_size_1,
+                             num_layers=self.stacked_layers,
+                             batch_first=True)
+        self.lstm2 = nn.LSTM(self.hidden_size_1,
+                             self.hidden_size_2,
+                             num_layers=self.stacked_layers,
+                             batch_first=True)
+
+        self.fc = nn.Linear(self.hidden_size_2, output_length)
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        batch_size, seq_len, _ = x.size()
+
+        hidden = self.init_hidden1(batch_size)
+        output, _ = self.lstm1(x, hidden)
+        output = F.dropout(output, p=self.dropout_probability, training=True)
+        state = self.init_hidden2(batch_size)
+        output, state = self.lstm2(output, state)
+        output = F.dropout(output, p=self.dropout_probability, training=True)
+        output = output[:, -1, :]
+        y_pred = self.fc(output)
+        return y_pred
+
+    def init_hidden1(self, batch_size):
+        hidden_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_1))
+        cell_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_1))
+        return hidden_state, cell_state
+
+    def init_hidden2(self, batch_size):
+        hidden_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_2))
+        cell_state = Variable(torch.zeros(self.stacked_layers, batch_size, self.hidden_size_2))
+        return hidden_state, cell_state
+
+    def loss(self, pred, truth):
+        return self.loss_fn(pred, truth)
+
+    def predict(self, X):
+        return self(torch.tensor(X, dtype=torch.float32)).view(-1).detach().numpy()
